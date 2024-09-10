555f9cc205bd283fbab9edf22158465f6e5d2029
Shripad Kulkarni <134084978+ShripadCoolkarni@users.noreply.github.com>
2024-09-10 11:57:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-const express= require("express");
-const mongoose=require("mongoose");
-const bodyParser= require("body-parser");
-const dotenv=require("dotenv");
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const app = express();
 dotenv.config();
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-const username=process.env.MONGODB_USERNAME
-const password=process.env.MONGODB_PASSWORD
-mongoose.connect(`mongodb+srv://${username}:${password}cluster0.e36nl.mongodb.net/RegistrationFormDBmongodb?retryWrites=true&w=majority
-`)
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.e36nl.mongodb.net/RegistrationFormDB?retryWrites=true&w=majority`)
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.log('MongoDB connection error:', err));
 
-
-const registerationSchema=new mongoose.Schema({
-    name:String,
-    email:String,
+const registrationSchema = new mongoose.Schema({
+    name: String,
+    email: String,
     password: String,
-})
-const Registration = mongoose.model("Registration",registerationSchema);
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname + "/pages/index.html");
-})
-app.post("/register", async (req,res)=>{
-    try{
-    const { name, email, password } = req.body;
-
-    const existingUser=await Registration.findOne({email:email});
-    if(!existingUser){
-        const registrationData= new Registration({
-            name,
-            email,
-            password,
-        });
-         await registrationData.save();
-         res.redirect("/success")
-        } 
-    
-    else{
-        console.log("user already exist")
-        res.redirect("/error")
-    } } 
-    catch (error){
-    console.log(error)
-    res.redirect("/error");
-    } 
 });
-app.get("/success",(req,res)=>{
-    res.sendFile(__dirname+"/pages/success.html");
-})
-app.get("/error",(req,res)=>{
-    res.sendFile(__dirname+"/pages/error.html");
-})
+const Registration = mongoose.model("Registration", registrationSchema);
 
-app.listen(port,()=>{console.log(`server running on port ${port}`);})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const existingUser = await Registration.findOne({ email: email });
+        if (!existingUser) {
+            const registrationData = new Registration({
+                name,
+                email,
+                password,
+            });
+            await registrationData.save();
+            res.redirect("/success");
+        } else {
+            console.log("User already exists");
+            res.redirect("/error");
+        }
+    } catch (error) {
+        console.log(error);
+        res.redirect("/error");
+    }
+});
+
+app.get("/success", (req, res) => {
+    res.sendFile(__dirname + "/pages/success.html");
+});
+
+app.get("/error", (req, res) => {
+    res.sendFile(__dirname + "/pages/error.html");
+});
+
+app.listen(port, () => { console.log(`Server running on port ${port}`); });
